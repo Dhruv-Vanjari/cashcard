1480e7ce08148a6c15d27afb5107f6a562b8d5cc
Dhruv-Vanjari <vanjaridhruv@gmail.com>
2024-05-10 01:27:52 +0530

    [Day 3] Lab: Repositories and Spring Data (58m)

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 // @RestController tells Spring that this class is a Component of type RestController and
 // capable of handling HTTP requests.
 // @RequestMapping is a companion to @RestController that indicates which address requests
@@ -14,12 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cashcards")
 class CashCardController {
 
+   private final CashCardRepository cashCardRepository;
+
+   private CashCardController(CashCardRepository cashCardRepository) {
+      this.cashCardRepository = cashCardRepository;
+   }
+
    @GetMapping("/{requestedId}")
    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
 
-      if (requestedId.equals(99L)) {
-         CashCard cashCard = new CashCard(99L, 123.45);
-         return ResponseEntity.ok(cashCard);
+      Optional<CashCard> cashCard = cashCardRepository.findById(requestedId);
+
+      if (cashCard.isPresent()) {
+         return ResponseEntity.ok(cashCard.get());
       } else {
          return ResponseEntity.notFound().build();
       }
